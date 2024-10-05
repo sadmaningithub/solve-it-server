1d6a5b5190e346058eee75eecbf5e6288aed7f57
sadmaningithub <sadmanahmed261@gmail.com>
2024-10-05 20:28:24 +0600

    delete operation created

--- index.js
+++ index.js
@@ -49,6 +49,12 @@ async function run() {
        res.send(result)
     })
 
+    app.delete('/assignments/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await assignmentCollection.deleteOne(query);
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
